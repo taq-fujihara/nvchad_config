f290b354bec27be14756893c2546df7e4534aca7
taq-fujihara <taq.fujihara@gmail.com>
2024-02-09 18:54:24 +0900

    more keymappings

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,13 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<leader>pt"] = { "" },
+    ["<leader>ph"] = { "" },
+  },
+}
+
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
@@ -13,12 +20,53 @@ M.general = {
       "formatting",
     },
 
+    -- basics
     ["<leader>w"] = { "<cmd> w <CR>", "Save file" },
     ["<leader>q"] = { "<cmd> q <CR>", "Close file" },
+    ["<leader>n"] = { "<cmd> enew <CR>", "New buffer" },
+    ["<leader>p"] = { "<cmd> Telescope commands <CR>", "Command Pallet" },
+
+    -- editing
+    ["<leader>j"] = { "*``cgn", "Replace word under cursor" },
+
+    -- panes
+    ["|"] = { "<cmd> vsplit <CR>", "Split Pane Vertically" },
+    ["-"] = { "<cmd> split <CR>", "Split Pane Horizontally" },
+    ["gt"] = {
+      function()
+        require("nvchad.tabufline").tabuflineNext()
+      end,
+      "Goto next buffer",
+    },
+    ["gT"] = {
+      function()
+        require("nvchad.tabufline").tabuflinePrev()
+      end,
+      "Goto prev buffer",
+    },
+
+    -- git
+    ["<leader>gg"] = { "<cmd> LazyGit <CR>", "LazyGit" },
+
+    ["<leader>tt"] = {
+      function()
+        require("base46").toggle_transparency()
+      end,
+      "Toggle Transparency",
+    },
   },
   v = {
     [">"] = { ">gv", "indent"},
   },
+}
+
+M.telescope = {
+  plugin = true,
+
+  n = {
+    ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
+    ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "Git status" },
+  }
 }
 
 M.nvimtree = {
@@ -31,7 +79,5 @@ M.nvimtree = {
     ["<leader>o"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
   },
 }
-
--- more keybinds!
 
 return M
